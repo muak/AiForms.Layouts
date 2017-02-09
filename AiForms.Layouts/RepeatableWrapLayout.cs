96745525254fcef6edfc1d85d8a6749e5760a4e9
kamu <kamuxxx@hotmail.com>
2017-02-09 23:40:19 +0900

    Having roughly completed

---
+++ AiForms.Layouts/RepeatableWrapLayout.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AiForms.Layouts
+{
+
+    /// <summary>
+    /// WrapLayout corresponding to DataTemplate
+    /// Base Code ->  https://forums.xamarin.com/discussion/21635/xforms-needs-an-itemscontrol/p2
+    /// </summary>
+    public class RepeatableWrapLayout : WrapLayout
+    {
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
+            propertyName: "ItemsSource",
+            returnType: typeof(IEnumerable),
+            declaringType: typeof(RepeatableWrapLayout),
+            defaultValue: null,
+            defaultBindingMode: BindingMode.OneWay,
+            propertyChanged: ItemsChanged);
+
+        public IEnumerable ItemsSource {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create(
+            propertyName: "ItemTemplate",
+            returnType: typeof(DataTemplate),
+            declaringType: typeof(RepeatableWrapLayout),
+            defaultValue: default(DataTemplate));
+
+        public DataTemplate ItemTemplate {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        public static BindableProperty ItemTapCommandProperty =
+            BindableProperty.Create(
+                nameof(ItemTapCommand),
+                typeof(ICommand),
+                typeof(RepeatableWrapLayout),
+                default(ICommand),
+                defaultBindingMode: BindingMode.OneWay
+            );
+        /// <summary>
+        /// Command invoked when it tapped a item.
+        /// </summary>
+        public ICommand ItemTapCommand {
+            get { return (ICommand)GetValue(ItemTapCommandProperty); }
+            set { SetValue(ItemTapCommandProperty, value); }
+        }
+
+        private static void ItemsChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            IEnumerable newValueAsEnumerable;
+            try {
+                newValueAsEnumerable = newValue as IEnumerable;
+            }
+            catch (Exception e) {
+                throw e;
+            }
+
+            var control = (RepeatableWrapLayout)bindable;
+            var oldObservableCollection = oldValue as INotifyCollectionChanged;
+
+            if (oldObservableCollection != null) {
+                oldObservableCollection.CollectionChanged -= control.OnItemsSourceCollectionChanged;
+            }
+
+            var newObservableCollection = newValue as INotifyCollectionChanged;
+
+            if (newObservableCollection != null) {
+                newObservableCollection.CollectionChanged += control.OnItemsSourceCollectionChanged;
+            }
+
+            control.Children.Clear();
+
+            if (newValueAsEnumerable != null) {
+                foreach (var item in newValueAsEnumerable) {
+                    var view = control.CreateChildViewFor(item);
+                    if (control.ItemTapCommand != null) {
+                        view.GestureRecognizers.Add(new TapGestureRecognizer {
+                            Command = control.ItemTapCommand,
+                            CommandParameter = item,
+                        });
+                    }
+                    control.Children.Add(view);
+                }
+            }
+
+            control.UpdateChildrenLayout();
+            control.InvalidateLayout();
+        }
+
+        private void OnItemsSourceCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            var invalidate = false;
+
+            if (e.Action == NotifyCollectionChangedAction.Replace) {
+
+                this.Children.RemoveAt(e.OldStartingIndex);
+
+                var item = e.NewItems[e.NewStartingIndex];
+                var view = CreateChildViewFor(item);
+
+                if (ItemTapCommand != null) {
+                    view.GestureRecognizers.Add(new TapGestureRecognizer {
+                        Command = ItemTapCommand,
+                        CommandParameter = item,
+                    });
+                }
+
+                this.Children.Insert(e.NewStartingIndex, view);
+            }
+
+            else if (e.Action == NotifyCollectionChangedAction.Add) {
+                if (e.NewItems != null) {
+                    for (var i = 0; i < e.NewItems.Count; ++i) {
+                        var item = e.NewItems[i];
+                        var view = this.CreateChildViewFor(item);
+
+                        if (ItemTapCommand != null) {
+                            view.GestureRecognizers.Add(new TapGestureRecognizer {
+                                Command = ItemTapCommand,
+                                CommandParameter = item,
+                            });
+                        }
+
+                        this.Children.Insert(i + e.NewStartingIndex, view);
+                    }
+                }
+            }
+
+            else if (e.Action == NotifyCollectionChangedAction.Remove) {
+                if (e.OldItems != null) {
+                    this.Children.RemoveAt(e.OldStartingIndex);
+                }
+            }
+
+            else if (e.Action == NotifyCollectionChangedAction.Reset) {
+                this.Children.Clear();
+            }
+
+            else {
+                return;
+            }
+
+            if (invalidate) {
+                this.UpdateChildrenLayout();
+                this.InvalidateLayout();
+            }
+
+        }
+
+        private View CreateChildViewFor(object item)
+        {
+            this.ItemTemplate.SetValue(BindableObject.BindingContextProperty, item);
+            return (View)this.ItemTemplate.CreateContent();
+        }
+    }
+
+}
+
--- Sample/Sample/ViewModels/MainPageViewModel.cs
+++ Sample/Sample/ViewModels/MainPageViewModel.cs
@@ -7,6 +7,8 @@ using Xamarin.Forms;
 using System.Net.Http;
 using Prism.Services;
 using System.IO;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
 
 namespace Sample.ViewModels
 {
@@ -15,23 +17,122 @@ namespace Sample.ViewModels
 
         private IPageDialogService _pageDialog;
         private INavigationService _navi;
-        public MainPageViewModel(INavigationService navigationService,IPageDialogService pageDlg)
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService pageDlg)
         {
             _navi = navigationService;
             _pageDialog = pageDlg;
+            Visible = false;
+
+            BoxList = new ObservableCollection<Hoge>(new List<Hoge> {
+                new Hoge{Name="ABC",Color=Color.Red},
+                new Hoge{Name="Defsd",Color=Color.Blue},
+                new Hoge{Name="あｓｄｆさ",Color=Color.Yellow},
+                new Hoge{Name="ｔｊｊｇｆｆｄ",Color=Color.Green},
+                new Hoge{Name="なむ",Color=Color.Lime},
+                new Hoge{Name="XYZ",Color=Color.Pink},
+                new Hoge{Name="DEF",Color=Color.Aqua},
+                new Hoge{Name="KISD",Color=Color.Olive},
+            });
         }
 
+        private bool _Visible;
+        public bool Visible {
+            get { return _Visible; }
+            set { SetProperty(ref _Visible, value); }
+        }
 
-        public void OnNavigatedFrom(NavigationParameters parameters){
+        public ObservableCollection<Hoge> BoxList { get; set; }
+
+        private DelegateCommand _VisibleChangeCommand;
+        public DelegateCommand VisibleChangeCommand {
+            get {
+                return _VisibleChangeCommand = _VisibleChangeCommand ?? new DelegateCommand(() => {
+                    Visible = !Visible;
+                });
+            }
+        }
+
+        private DelegateCommand _ClearCommand;
+        public DelegateCommand ClearCommand {
+            get { return _ClearCommand = _ClearCommand ?? new DelegateCommand(() => {
+                BoxList.Clear();
+            }); }
+        }
+
+        private DelegateCommand _ReloadCommand;
+        public DelegateCommand ReloadCommand {
+            get { return _ReloadCommand = _ReloadCommand ?? new DelegateCommand(() => {
+                var list = new List<Hoge> {
+                    new Hoge{Name="afsf",Color=Color.Orange},
+                    new Hoge{Name="Desfsffsd",Color=Color.Blue},
+                    new Hoge{Name="dsfs",Color=Color.Yellow},
+                    new Hoge{Name="ｔｊｊｇｆｆｄ",Color=Color.Green},
+                    new Hoge{Name="なむ",Color=Color.Lime},
+                    new Hoge{Name="XYZ",Color=Color.Pink},
+                    new Hoge{Name="DEF",Color=Color.Aqua},
+                    new Hoge{Name="KISD",Color=Color.Maroon},
+                    };
+
+                foreach (var c in list) {
+                    BoxList.Add(c);
+                }
+
+            }); }
+        }
+
+        private DelegateCommand _DeleteCommand;
+        public DelegateCommand DeleteCommand {
+            get { return _DeleteCommand = _DeleteCommand ?? new DelegateCommand(() => {
+                BoxList.RemoveAt(0);
+            }); }
+        }
+
+        private DelegateCommand _ReplaceCommand;
+        public DelegateCommand ReplaceCommand {
+            get { return _ReplaceCommand = _ReplaceCommand ?? new DelegateCommand(() => {
+                BoxList[0] = new Hoge {
+                    Name="ReplaceItem",Color = Color.Teal
+                };
+                //BoxList[0].Name = "Replace";
+                //BoxList[0].Color = Color.Teal;
+            }); }
+        }
+
+        private DelegateCommand<object> _TapCommand;
+        public DelegateCommand<object> TapCommand {
+            get { return _TapCommand = _TapCommand ?? new DelegateCommand<object>((x) => {
+                var item = x as Hoge;
+                _pageDialog.DisplayAlertAsync("",item.Name,"OK");
+            }); }
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
 
         }
 
-        public void OnNavigatedTo(NavigationParameters parameters){
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
 
         }
 
         public void OnNavigatingTo(NavigationParameters parameters)
         {
+        }
+
+        public class Hoge:BindableBase
+        {
+           private string _Name;
+            public string Name {
+                get { return _Name; }
+                set { SetProperty(ref _Name, value); }
+            }
+
+            private Color _Color;
+            public Color Color {
+                get { return _Color; }
+                set { SetProperty(ref _Color, value); }
+            }
         }
     }
 }
